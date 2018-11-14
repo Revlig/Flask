71ab32cb195388a8893bf53b78ab743671dd9fa6
Gilver <gilver63@gmail.com>
2018-11-14 02:55:39 -0200

    [flask game] - Insert html files.

--- game.py
+++ game.py
@@ -1,21 +1,39 @@
 """My Flask game."""
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Gilver Souza',
+        'title': 'Flask Game 1',
+        'content': 'First post content',
+        'date_posted': 'November 10, 2018'
+    },
+    {
+        'author': 'Alan Turing',
+        'title': 'Flask Game 2',
+        'content': 'Second post content',
+        'date_posted': 'November 10, 1954'
+    }
+]
+
 
 @app.route("/")  # '/' root is home too
 @app.route("/home")
 def hello():
-    return "<strong>Hello World!</strong>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/login")
 def login():
-    return "Login Area"
+    return render_template('login.html', title='Sign Area')
 
 
 @app.route("/about")
 def about():
-    return 'About Us'
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
